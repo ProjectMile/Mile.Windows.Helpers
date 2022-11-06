9bbbea39ed149ecef7680e15aed4fdd946f2a29d
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-06 14:04:56 +0800

    Add MileIsWindowsVersionAtLeast function.

--- Mile.Helpers/Mile.Helpers.cpp
+++ Mile.Helpers/Mile.Helpers.cpp
@@ -9,3 +9,28 @@
  */
 
 #include "Mile.Helpers.h"
+
+EXTERN_C BOOL WINAPI MileIsWindowsVersionAtLeast(
+    _In_ DWORD Major,
+    _In_ DWORD Minor,
+    _In_ DWORD Build)
+{
+    OSVERSIONINFOEXW OSVersionInfoEx = { 0 };
+    OSVersionInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+    OSVersionInfoEx.dwMajorVersion = Major;
+    OSVersionInfoEx.dwMinorVersion = Minor;
+    OSVersionInfoEx.dwBuildNumber = Build;
+    return ::VerifyVersionInfoW(
+        &OSVersionInfoEx,
+        VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER,
+        ::VerSetConditionMask(
+            ::VerSetConditionMask(
+                ::VerSetConditionMask(
+                    0,
+                    VER_MAJORVERSION,
+                    VER_GREATER_EQUAL),
+                VER_MINORVERSION,
+                VER_GREATER_EQUAL),
+            VER_BUILDNUMBER,
+            VER_GREATER_EQUAL));
+}
--- Mile.Helpers/Mile.Helpers.h
+++ Mile.Helpers/Mile.Helpers.h
@@ -15,4 +15,18 @@
 
 #include <Windows.h>
 
+/**
+ * @brief Indicates if the current OS version matches, or is greater than, the
+ *        provided version information.
+ * @param Major The major version number of the operating system.
+ * @param Minor The minor version number of the operating system.
+ * @param Build The build number of the operating system.
+ * @return TRUE if the specified version matches, or is greater than, the
+ *         version of the current Windows operating system; otherwise, FALSE.
+*/
+EXTERN_C BOOL WINAPI MileIsWindowsVersionAtLeast(
+    _In_ DWORD Major,
+    _In_ DWORD Minor,
+    _In_ DWORD Build);
+
 #endif // !MILE_WINDOWS_HELPERS
