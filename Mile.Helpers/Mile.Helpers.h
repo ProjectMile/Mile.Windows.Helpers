767b670d8296d0ed0b23dabc7632235162359b82
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-06 14:03:02 +0800

    Add Mile.Helpers implementation.

---
+++ Mile.Helpers/Mile.Helpers.h
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Helpers.h
+ * PURPOSE:   Definition for the essential Windows helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef MILE_WINDOWS_HELPERS
+#define MILE_WINDOWS_HELPERS
+
+#include <Windows.h>
+
+#endif // !MILE_WINDOWS_HELPERS
