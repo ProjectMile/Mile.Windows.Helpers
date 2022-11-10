a87bef26dbb08e4be45cfb2b6209cf0236452f7a
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-10 23:03:51 +0800

    Improve the MileIsWindowsVersionAtLeast implementation. (Thanks to wangwenx190.)

--- Mile.Helpers/Mile.Helpers.cpp
+++ Mile.Helpers/Mile.Helpers.cpp
@@ -15,6 +15,42 @@
 
 namespace
 {
+    static LPOSVERSIONINFOW GetWindowsVersionInformation()
+    {
+        static LPOSVERSIONINFOW CachedResult = ([]() -> LPOSVERSIONINFOW
+        {
+            static OSVERSIONINFOW VersionInformation = { 0 };
+            VersionInformation.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
+
+            HMODULE ModuleHandle = ::GetModuleHandleW(L"ntdll.dll");
+            if (ModuleHandle)
+            {
+                // Reference: https://learn.microsoft.com/en-us/windows-hardware
+                //            /drivers/ddi/wdm/nf-wdm-rtlgetversion
+                typedef NTSTATUS(NTAPI* ProcType)(PRTL_OSVERSIONINFOW);
+
+                ProcType ProcAddress = reinterpret_cast<ProcType>(
+                    ::GetProcAddress(ModuleHandle, "RtlGetVersion"));
+                if (ProcAddress)
+                {
+                    // Use constant 0 due to STATUS_SUCCESS not defined in the
+                    // standard Windows SDK.
+                    if (0 == ProcAddress(&VersionInformation))
+                    {
+                        return &VersionInformation;
+                    }
+                }
+            }
+
+            // No fallback solution due to RtlGetVersion has been introduced
+            // since Windows 2000 and no interest to support earlier Windows
+            // version than that.
+            return nullptr;
+        }());
+
+        return CachedResult;
+    }
+
     static bool IsPrivatePerMonitorSupportExtensionApplicable()
     {
         static bool CachedResult = ([]() -> bool
@@ -89,24 +125,25 @@ EXTERN_C BOOL WINAPI MileIsWindowsVersionAtLeast(
     _In_ DWORD Minor,
     _In_ DWORD Build)
 {
-    OSVERSIONINFOEXW OSVersionInfoEx = { 0 };
-    OSVersionInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
-    OSVersionInfoEx.dwMajorVersion = Major;
-    OSVersionInfoEx.dwMinorVersion = Minor;
-    OSVersionInfoEx.dwBuildNumber = Build;
-    return ::VerifyVersionInfoW(
-        &OSVersionInfoEx,
-        VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER,
-        ::VerSetConditionMask(
-            ::VerSetConditionMask(
-                ::VerSetConditionMask(
-                    0,
-                    VER_MAJORVERSION,
-                    VER_GREATER_EQUAL),
-                VER_MINORVERSION,
-                VER_GREATER_EQUAL),
-            VER_BUILDNUMBER,
-            VER_GREATER_EQUAL));
+    LPOSVERSIONINFOW VersionInformation = ::GetWindowsVersionInformation();
+    if (!VersionInformation)
+    {
+        return FALSE;
+    }
+
+    // Reference: https://github.com/dotnet/runtime/blob
+    //            /db213657acc53fc48212867d5728e83d9e36a558
+    //            /src/libraries/System.Private.CoreLib
+    //            /src/System/OperatingSystem.cs#L308
+    if (VersionInformation->dwMajorVersion != Major)
+    {
+        return (VersionInformation->dwMajorVersion > Major);
+    }
+    if (VersionInformation->dwMinorVersion != Minor)
+    {
+        return (VersionInformation->dwMinorVersion > Minor);
+    }
+    return (VersionInformation->dwBuildNumber >= Build);
 }
 
 EXTERN_C BOOL WINAPI MileEnablePerMonitorDialogScaling()
