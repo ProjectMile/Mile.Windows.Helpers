c6bfe80ec6ea7114a45c29767b7edb9685711f47
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-02 23:47:55 +0800

    Add Mile.Helpers.CppWinRT.

---
+++ Mile.Helpers/Mile.Helpers.CppWinRT.h
@@ -0,0 +1,23 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Helpers.CppWinRT.h
+ * PURPOSE:   Definition for the essential Windows helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef MILE_WINDOWS_HELPERS_CPPWINRT
+#define MILE_WINDOWS_HELPERS_CPPWINRT
+
+#include <winrt/Windows.Foundation.h>
+
+namespace Mile::WinRT
+{
+    
+}
+
+#endif // !MILE_WINDOWS_HELPERS_CPPWINRT
