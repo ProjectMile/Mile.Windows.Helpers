691ccf3b3679d39496f7ddec816e7d1173a558c3
MouriNaruto <KurikoMouri@outlook.jp>
2023-11-06 20:07:30 +0800

    Add Mile.Helpers.CppBase.

---
+++ Mile.Helpers/Mile.Helpers.CppBase.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Helpers.CppBase.cpp
+ * PURPOSE:   Implementation for the essential Windows helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: MouriNaruto (KurikoMouri@outlook.jp)
+ */
+
+#include "Mile.Helpers.CppBase.h"
---
+++ Mile.Helpers/Mile.Helpers.CppBase.h
@@ -0,0 +1,21 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Helpers.CppBase.h
+ * PURPOSE:   Definition for the essential Windows helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: MouriNaruto (KurikoMouri@outlook.jp)
+ */
+
+#pragma once
+
+#ifndef MILE_WINDOWS_HELPERS_CPPBASE
+#define MILE_WINDOWS_HELPERS_CPPBASE
+
+namespace Mile
+{
+    
+}
+
+#endif // !MILE_WINDOWS_HELPERS_CPPBASE
