9037f33d2fbcb521be6147c648a120ad08e280ae
MouriNaruto <Kenji.Mouri@outlook.com>
2024-04-23 20:54:59 +0800

    Add Mile.Helpers.RefreshPackageVersion project.

---
+++ Mile.Helpers.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Mile.Helpers.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "Mile.Helpers.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
